d707acf5c065d74876df4bb9a0c66bfb3ccea80d
bharat2005 <bharatvdeshm2005@gmail.com>
2025-06-28 02:00:05 +0530

    envexample

--- config/firebase.js
+++ config/firebase.js
@@ -8,17 +8,16 @@ import { getFirestore } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: Constants.expoConfig.extra.FIREBASE_API_KEY,
-  authDomain: "mizzy-253f0.firebaseapp.com",
-  projectId: Constants.expoConfig.extra.FIREBASE_PROJECT_ID,
-  storageBucket: "mizzy-253f0.firebasestorage.app",
-  messagingSenderId: "500333803394",
-  appId: Constants.expoConfig.extra.FIREBASE_APP_ID,
-  measurementId: "G-ZMPBMVRMS2"
+  apiKey: Constants.expoConfig.extra.API_KEY,
+  authDomain: Constants.expoConfig.extra.AUTH_DOMAIN,
+  projectId: Constants.expoConfig.extra.PROJECT_ID,
+  storageBucket: Constants.expoConfig.extra.STORAGE_BUCKET,
+  messagingSenderId: Constants.expoConfig.extra.MESSAGING_SENDER_ID,
+  appId: Constants.expoConfig.extra.APP_ID,
+  measurementId:  Constants.expoConfig.extra.MEASURMENT_ID,
 };
 
 // Initialize Firebase
